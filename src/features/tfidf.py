Correct TFIDF formula

src/features/tfidf.py
@@ -4,7 +4,8 @@ from math import log
 
 class Tfidf:
     @staticmethod
-    def get_tf(data: List[str]) -> Dict[str, int]:
+    def get_tf(data: List[str], doc_idx: List[str]) -> Dict[str, int]:
+        n_docs = len(set(doc_idx))
         corpus = ' '.join(data)
         tf = {}
         for word in corpus.split():
@@ -12,21 +13,21 @@ class Tfidf:
                 tf[word] = 1
             else:
                 tf[word] += 1
-                
-        return tf
+        
+        return { word: count / n_docs for (word, count) in tf.items()}
     
     @staticmethod
     def get_idf(data: List[str], word_list: List[str]) -> Dict[str, float]:
         idf = {}
         N = len(data)
         for word in word_list:
-            idf[word] = N / log(Tfidf.get_df(word, data) + 1)
+            idf[word] = log(N / Tfidf.get_df(word, data)) + 1
             
         return idf
     
     @staticmethod
-    def get_tfidf(data: List[str]) -> Dict[str, float]:
-        tf = Tfidf.get_tf(data)
+    def get_tfidf(data: List[str], doc_idx: List[str]) -> Dict[str, float]:
+        tf = Tfidf.get_tf(data, doc_idx)
         word_list = [key for key in tf]
         idf = Tfidf.get_idf(data, word_list)
         