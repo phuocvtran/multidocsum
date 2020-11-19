31a9966e1822b91efe65bcb5f52cffdfcc92da7b
Trần Vĩnh Phước <vinhphuoc9919@gmail.com>
2020-11-19 14:02:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="multidoc-dev", packages=['src',
-                                     'src.data',
-                                     'src.features',
-                                     'src.models'])
+
+
+setup(name='multidocsum', 
+      version="0.0.1",
+      packages=['src',
+                'src.data',
+                'src.features',
+                'src.models'],
+      python_requires='==3.7.*', 
+      install_requires=['nltk==3.5',
+                        'numpy==1.19.4',
+                        'regex==2020.11.13',
+                        'scikit-learn==0.21.3',
+                        'scipy==1.5.4',
+                        'underthesea==1.2.2',
+                        'pyvi==0.1',
+                        'pandas==1.1.4'])
